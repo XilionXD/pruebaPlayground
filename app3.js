c4d51928ff493bce5357c378d14c462e41f997e4
Amadeo Abud <amaabud123@gmail.com>
2023-07-01 16:06:53 -0300

    Cambio app3

--- app3.js
+++ app3.js
@@ -1,1 +1,9 @@
 Mi primer commit
+let auto = function(contar){
+    if(contar === 0){
+        console.log(Inexistente)
+    }else{
+        console.log(BuscandoVehiculo)
+    }
+}
+console.log(auto(20))
